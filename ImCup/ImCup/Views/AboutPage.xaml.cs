bdd85da710bc924e5f2347134a715c64ad05a7ec
maxUo <crashsiti@yandex.ru>
2017-03-28 23:28:26 +0300

    Add project files.

---
+++ ImCup/ImCup/Views/AboutPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿
+using Xamarin.Forms;
+
+namespace ImCup.Views {
+    public partial class AboutPage : ContentPage {
+        public AboutPage() {
+            InitializeComponent ();
+        }
+    }
+}
