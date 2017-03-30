0f854d682fb97060b78a19bc802243c23196e4de
maxUo <crashsiti@yandex.ru>
2017-03-30 22:50:27 +0300

    228 commit

--- ImCup/ImCup/Scene1/SecondPage.xaml.cs
+++ ImCup/ImCup/Scene1/SecondPage.xaml.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DeviceMotion.Plugin;
+using DeviceMotion.Plugin.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ImCup.Scene1 {
     [XamlCompilation (XamlCompilationOptions.Compile)]
     public partial class SecondPage : ContentPage {
-        public SecondPage() {
-            InitializeComponent ();
+        public SecondPage()
+        {
+            InitializeComponent();
+            CrossDeviceMotion.Current.Start(MotionSensorType.Accelerometer);
+
+            CrossDeviceMotion.Current.SensorValueChanged += (s, a) =>
+            {
+                lbGyro.Text =
+                    $"A: {((MotionVector) a.Value).X},{((MotionVector) a.Value).Y},{((MotionVector) a.Value).Z}";
+            };
+
         }
 
         private async void Button_OnClicked(object sender, EventArgs e)
@@ -21,7 +32,8 @@ namespace ImCup.Scene1 {
 
         private void Button_On(object sender, EventArgs e)
         {
-            this.AnimationView.Play();
+            
+            //this.AnimationView.Play();
         }
     }
 }
