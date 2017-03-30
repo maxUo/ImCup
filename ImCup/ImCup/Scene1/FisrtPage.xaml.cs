af55ec481d5e28cfe76b4f4d2b936d6599766659
maxUo <crashsiti@yandex.ru>
2017-03-31 00:20:07 +0300

    1

--- ImCup/ImCup/Scene1/FisrtPage.xaml.cs
+++ ImCup/ImCup/Scene1/FisrtPage.xaml.cs
@@ -9,11 +9,18 @@ using Xamarin.Forms.Xaml;
 
 namespace ImCup.Scene1 {
     [XamlCompilation (XamlCompilationOptions.Compile)]
-    public partial class FisrtPage : ContentPage {
+    public partial class FisrtPage : ContentPage
+    {
+        private List<DataSource> dataSource;
         public FisrtPage() {
             
             InitializeComponent ();
-            
+            dataSource = new List<DataSource>()
+            {
+                new DataSource() { ViewForPresent = new FirstView()}
+            };
+            lw.ItemsSource = dataSource;
+
         }
 
         private void StartAnimation_OnClicked(object sender, EventArgs e)
@@ -24,7 +31,12 @@ namespace ImCup.Scene1 {
         private async void Button_OnClicked(object sender, EventArgs e)
         {
             
-            await Navigation.PushModalAsync(new Scene1.SecondPage());
+            //await Navigation.PushModalAsync(new Scene1.SecondPage());
         }
+    }
+
+    public class DataSource
+    {
+        public View ViewForPresent { get; set; }
     }
 }
